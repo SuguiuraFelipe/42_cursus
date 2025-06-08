d6c7ddaec1075616683a5ac6dd4c1865416cbf0e
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2025-06-08 09:49:49 +0100

    Minitalk

---
+++ minitalk/inc/minitalk.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsuguiur <fsuguiur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/08 09:20:27 by fsuguiur          #+#    #+#             */
+/*   Updated: 2025/06/08 09:20:28 by fsuguiur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../ft_printf/ft_printf.h"
+# include <signal.h>
+
+// server functions
+
+unsigned char   bits_to_char(int *arr_bits);
+void    signal_to_bits(int signal);
+
+// client functions
+
+int	av_to_pid(char	*av);
+int	*char_to_bits(unsigned char c);
+void	msg_to_server(int sv_pid, int *bits);
+
+#endif
